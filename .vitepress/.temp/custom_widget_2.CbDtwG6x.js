94f519f40975770ae66e6a2962ae8b55888646d4
haade-administrator <administrateur@haade.fr>
2026-04-05 21:20:48 +0200

    remove vitepress init

--- .vitepress/.temp/custom_widget_2.CbDtwG6x.js
+++
@@ -1,6 +0,0 @@
-const _imports_0 = "/Panel-Project-Haade-DOCS/images/custom_widget_1.png";
-const _imports_1 = "/Panel-Project-Haade-DOCS/images/custom_widget_2.png";
-export {
-  _imports_0 as _,
-  _imports_1 as a
-};
