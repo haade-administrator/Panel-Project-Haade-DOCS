39089db5e17603792d934fb4a8c349fb07a42fac
haade-administrator <administrateur@haade.fr>
2026-04-05 21:01:04 +0200

    change error github actions

--- .vitepress/config/index.js
+++ .vitepress/config/index.js
@@ -15,6 +15,14 @@ export default defineConfig({
 
   lastUpdated: true,
   cleanUrls: true,
+
+  vite: {
+  define: {
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_OPTIONS_API__: true,
+    __INTLIFY_PROD_DEVTOOLS__: false
+  }
+},
   
   vue: {
     template: {
